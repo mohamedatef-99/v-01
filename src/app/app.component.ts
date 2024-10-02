2f8ea1db3ff2b190bd9110d1d48a9bd39164bab8
Mohamed Atef <mohammed.atef.saif@gmail.com>
2024-10-02 17:26:51 +0300

    added package upgrade added header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderComponent } from './components/loader/loader.component';
+import { HeroComponent } from "./components/hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoaderComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'v-01';
+  title = 'Mohamed Atef';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoaderComponent } from './components/loader/loader.component';
+import { HeroComponent } from './components/hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: LoaderComponent,
+  //   title: 'Loading'
+  // },
+  {
+    path: '',
+    component: HeroComponent,
+    title: 'Mohamed Atef'
+  }
+];
