5bc863e45ff4f95bc0c207ba341ce5479d63240b
Mohamed Atef <mohammed.atef.saif@gmail.com>
2024-10-03 21:42:39 +0300

    Edit project structure

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from './home/home.component';
-import { SideComponent } from "./side/side.component";
-import { LeftSideComponent } from "./left-side/left-side.component";
-import { AboutComponent } from '../about/about.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [HeaderComponent, HomeComponent, SideComponent, LeftSideComponent, AboutComponent],
+  imports: [HomeComponent, AboutComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
